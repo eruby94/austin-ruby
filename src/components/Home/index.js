effaf357086b728789a4f215009b12d3de7ca6eb
Evan Ruby <evanruby94@gmail.com>
2018-04-20 15:14:06 -0400

    Home page redesign, added social media icons

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,21 +2,27 @@ import React from 'react'
 
 export default props => (
   <section id="intro">
-    <div id="img-container">
-      <img src={props.content.avatarImage.url} alt={props.content.avatarImage.alt} />
-    </div>
     <div id="about-container">
       {props.content.paragraphs.map((paragraph, index) => (
         <p key={index}>{paragraph}</p>
       ))}
     </div>
+    <div id="img-container">
+      <img src={props.content.avatarImage.url} alt={props.content.avatarImage.alt} />
+      <div id="social-icons">
+        <a href="mailto:austinjruby@gmail.com"><i className="fa fa-3x fa-envelope-square" /></a>
+        <i className="fab fa-3x fa-facebook-square" />
+        <i className="fab fa-3x fa-twitter-square" />
+        <i className="fab fa-3x fa-instagram" />
+      </div>
+    </div>
     <style jsx>{`
       #intro {
         display: flex;
-        flex-direction: column;
         align-items: center;
+        justify-content: space-between;
         padding: 30px 30px 0;
-        min-height: auto;
+        min-height: calc(100vh - 96px);
       }
       #img-container {
         width: 40%;
@@ -24,6 +30,7 @@ export default props => (
       }
       #about-container {
         width: 55%;
+        line-height: 26px;
       }
       img {
         width: 100%;
@@ -31,6 +38,41 @@ export default props => (
       }
       h1 {
         margin-top: 0;
+      }
+      #mail-button {
+        padding: 10px 16px;
+        font-family: 'Prociono';
+        background-color: var(--maroon);
+        color: #ffffff;
+        border-radius: 5px;
+      }
+      #social-icons {
+        margin: 40px 0 0;
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        width: 100%;
+      }
+      #social-icons i {
+        margin: 0 25px;
+        /* transition: 0.3s; */
+        cursor: pointer;
+        opacity: 0.5;
+      }
+      i:hover {
+        opacity: 1 !important;
+      }
+      .fa-envelope-square {
+        color: #0000ee;
+      }
+      .fa-facebook-square {
+        color: #3b5998;
+      }
+      .fa-twitter-square {
+        color: #1da1f2
+      }
+      .fa-instagram {
+        color: #c32aa3
       }
       @media screen and (max-width: 1023px) {
         #img-container {
@@ -41,12 +83,18 @@ export default props => (
         }
       }
       @media screen and (max-width: 640px) {
+        #intro {
+          flex-direction: column;
+        }
         #img-container {
           width: 100%;
           margin: 0;
         }
         #about-container {
           width: 100%;
+        }
+        #social-icons {
+          margin-bottom: 40px;
         }
       }
     `}</style>
